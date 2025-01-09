1b3fb320626294d5e843ddc20b1ed793bbe25644
Crazycoders283 <crazycoders283@gmail.com>
2025-01-09 22:30:47 +0530

    due

--- resources/js/Pages/src/components/Cards/PopularCard.jsx
+++ resources/js/Pages/src/components/Cards/PopularCard.jsx
@@ -1,63 +1,28 @@
-import React from 'react'
-import "../Cards/card.css";
-import { Card ,Stack} from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import "./Cards.css";
 
-const PopularCard = ({val}) => {
+const Cards = ({ destinations }) => {
   return (
-    <>
-        <Card className="rounded-2 shadow-sm popular">
-              <Card.Img
-                variant="top"
-                src={val.image}
-                className="img-fluid"
-                alt={"image"}
-              />
-              <Card.Body>
-              
-                <Card.Text>
-                  <i className="bi bi-geo-alt"></i>
-                  <span className="text">{val.location}</span>
-                </Card.Text>
+    <div className="card-grid">
+      {destinations.map((destination) => (
+        <div className="card" key={destination.id}>
+          <img
+            src={destination.image}
+            alt={destination.name}
+            className="card-image"
+          />
+          <div className="card-content">
+            <div className="card-header">
+              <h3>{destination.name}</h3>
+              <span className="rating">⭐ {destination.rating}</span>
+            </div>
+            <p>Starts from {destination.price}</p>
+            <button className="book-button">Book Now</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-                <Card.Title><NavLink className="body-text text-dark text-decoration-none" to="/tour-details"> {val.title} </NavLink></Card.Title>
-                <p className="reviwe">
-                  <span>
-                    <i className="bi bi-star-fill me-1"></i>
-                  </span>
-                  <span>{val.rating} </span>
-                  <span>( {val.reviews} reviews )</span>
-                </p>
-                {val.category.map((cat, index)=>{
-                  return(
-                    <span key={index} 
-                    className={cat.replace(/ .*/, "") + " badge"}>{cat}</span>
-                  )
-                })}
-                
-              </Card.Body>
-
-              <Card.Footer className="py-4">
-                {val.afterDiscount ? (
-                  <p className="text-decoration-line-through"> ${val.price.toFixed(2)}</p>
-                ): ""}
-               
-                <Stack
-                  direction="horizontal"
-                  className="justify-content-between  mt-3"
-                >
-                  <p>
-                    From <b>{val.afterDiscount ? val.afterDiscount.toFixed(2) : val.price.toFixed(2)}</b>
-                  </p>
-                  <p>
-                  
-                    <i className="bi bi-clock"></i> {val.days}
-                  </p>
-                </Stack>
-              </Card.Footer>
-            </Card>
-    </>
-  )
-}
-
-export default PopularCard
+export default Cards;
--- resources/js/Pages/src/pages/Home/Home.jsx
+++ resources/js/Pages/src/pages/Home/Home.jsx
@@ -2,12 +2,55 @@ import React from "react";
 import Banner from "../../components/Banner/Banner";
 import Features from "../../components/Features/Features";
 import { Container, Row, Col } from "react-bootstrap";
-import ValuedPartners from "./ValuedPartners"; // Import the ValuedPartners component
-import CruiseDeals from "./CruiseDeals"; // Import the CruiseDeals component
+import ValuedPartners from "./ValuedPartners";
+import CruiseDeals from "./CruiseDeals";
+import Cards from "../../components/Cards/Cards";
 import "./home.css";
 
-import { popularsData } from "../../utils/data";
-import PopularCard from "../../components/Cards/PopularCard";
+const destinations = [
+  {
+    id: 1,
+    name: "Vancouver BC",
+    price: "$64/p",
+    image: "https://images.hdqwalls.com/download/vancouver-city-night-3840x2160.jpg", // Replace with actual image URLs
+    rating: "5.0",
+  },
+  {
+    id: 2,
+    name: "Texas USA",
+    price: "$72/p",
+    image: "https://www.tripsavvy.com/thmb/uqNzAoqBIraz1RIKIPklL3H-z5Y=/2270x1321/filters:fill(auto,1)/GettyImages-629829924-5bdb57f74cedfd0026ae431f.jpg",
+    rating: "5.0",
+  },
+  {
+    id: 3,
+    name: "Florida",
+    price: "$85/p",
+    image: "https://media.timeout.com/images/105496849/image.jpg",
+    rating: "5.0",
+  },
+  {
+    id: 4,
+    name: "San Diego",
+    price: "$77/p",
+    image: "https://media.timeout.com/images/105496849/image.jpg",
+    rating: "5.0",
+  },
+  {
+    id: 5,
+    name: "New York",
+    price: "$96/p",
+    image: "https://vancouver.ca/images/cov/feature/about-vancouver-facebook-size.jpg",
+    rating: "5.0",
+  },
+  {
+    id: 6,
+    name: "Los Angeles",
+    price: "$127/p",
+    image: "https://vancouver.ca/images/cov/feature/about-vancouver-facebook-size.jpg",
+    rating: "5.0",
+  },
+];
 
 const Home = () => {
   return (
@@ -15,47 +58,23 @@ const Home = () => {
       <Banner />
       <Features />
 
-      {/* Tour Section Start */}
+      {/* Tour Section */}
       <section className="popular py-5">
         <Container>
           <Row>
             <Col md="12">
-              <div className="main_heading">
-                <h1>Choose Your Destination</h1>
+              <div className="main_heading text-center">
+                <h1>Explore By Destination</h1>
+                <p className="subtitle">
+                  Discover the top destinations curated just for you
+                </p>
               </div>
             </Col>
           </Row>
-          <Row>
-            {popularsData.map((val, inx) => (
-              <Col md={3} sm={6} xs={12} className="mb-5" key={inx}>
-                <PopularCard val={val} />
-              </Col>
-            ))}
-          </Row>
+          <Cards destinations={destinations} />
         </Container>
       </section>
-      {/* Tour Section End */}
-
-      {/* Popular Activities Section Start */}
-      <section className="popular py-5">
-        <Container>
-          <Row>
-            <Col md="12">
-              <div className="main_heading">
-                <h1>Popular Activities</h1>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            {popularsData.map((val, inx) => (
-              <Col md={3} sm={6} xs={12} className="mb-5" key={inx}>
-                <PopularCard val={val} />
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </section>
-      {/* Popular Activities Section End */}
+      
 
       {/* Cruise Deals Section */}
       <CruiseDeals />
