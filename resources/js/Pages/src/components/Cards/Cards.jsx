c3be9c71e2e45104e8e2192e2ff54ec94f22f391
Crazycoders283 <crazycoders283@gmail.com>
2025-01-10 22:12:08 +0530

    due

--- resources/js/Pages/src/components/Cards/Cards.jsx
+++ resources/js/Pages/src/components/Cards/Cards.jsx
@@ -1,25 +1,56 @@
 import React from "react";
-import { Card, Button, Row, Col } from "react-bootstrap";
 import "../Cards/card.css";
 
-const Cards = ({ data }) => {
+const cardsData = [
+  {
+    id: 1,
+    image: "http://vacay.ca/wp-content/uploads/2019/07/Vancouver-skyline.jpg",
+    title: "Carnival",
+    rating: "5.0",
+    description: "Starts from $31.27/p.p",
+    buttonText: "Book Now",
+  },
+  {
+    id: 2,
+    image: "https://d3e1m60ptf1oym.cloudfront.net/a23dbaca-c025-4f8d-a5c3-f115b1702532/18-08-2015-1680_xgaplus.jpg",
+    title: "Royal Caribbean",
+    rating: "4.8",
+    description: "Starts from $45.00/p.p",
+    buttonText: "Book Now",
+  },
+  {
+    id: 3,
+    image: "https://vancouver.ca/images/cov/feature/about-vancouver-facebook-size.jpg",
+    title: "Disney Cruise",
+    rating: "4.7",
+    description: "Starts from $50.00/p.p",
+    buttonText: "Book Now",
+  },
+];
+
+const App = () => {
   return (
-    <Row>
-      {data.map((item) => (
-        <Col key={item.id} sm={12} md={6} lg={4} className="mb-4">
-          <Card className="custom-card">
-            <Card.Img variant="top" src={item.image} alt={item.name} />
-            <Card.Body>
-              <Card.Title>{item.name}</Card.Title>
-              <Card.Text>Starts from {item.price}</Card.Text>
-              <div className="rating">⭐ {item.rating}</div>
-              <Button variant="primary">Book Now</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <div className="main-container">
+      <h1 className="heading">Our Cruises</h1>
+      <div className="card-grid">
+        {cardsData.map((card) => (
+          <div className="card" key={card.id}>
+            <div className="image-container">
+              <img src={card.image} alt={card.title} className="card-image" />
+              <div className="rating-badge">
+                <span>⭐ {card.rating}</span>
+              </div>
+            </div>
+            <div className="card-body">
+              <h3 className="card-title">{card.title}</h3>
+              <p className="card-description">{card.description}</p>
+              <button className="book-button">{card.buttonText}</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
-export default Cards;
+export default App;
