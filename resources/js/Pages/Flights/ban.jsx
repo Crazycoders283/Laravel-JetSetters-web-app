bb5c8db1053a7150db4c15c529f3f6e195f9a58d
Crazycoders283 <crazycoders283@gmail.com>
2025-02-25 23:58:16 +0530

    Update ban.jsx

--- resources/js/Pages/Flights/ban.jsx
+++ resources/js/Pages/Flights/ban.jsx
@@ -1,119 +1,8 @@
-// import React from "react";
-// import { Carousel } from "react-bootstrap";
-// import flightBanner from "../src/components/Banner/2.svg";
-// import Header from "../src/components/Common/Header/Header";
-// import Footer from "../src/components/Common/Footer/Footer";
-// import "./landing.css";
-// import { Link } from "@inertiajs/react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { 
-//   faPlaneDeparture, 
-//   faCalendarAlt, 
-//   faUsers, 
-//   faSearch,
-//   faCube 
-// } from "@fortawesome/free-solid-svg-icons";
-
-// const FlightsLanding = () => {
-//   return (
-//     <>
-//       {/* Header */}
-//       <Header />
-      
-//       {/* Flight Slider Section */}
-//       <section className="flight-slider">
-//         <Carousel variant="white">
-//           <Carousel.Item>
-//             <img src={flightBanner} className="d-block w-100" alt="Flight Search" />
-//             <Carousel.Caption>
-//               <div className="slider-description">
-//                 <h2 className="sub-text">Explore and Travel</h2>
-//                 <p className="heading">Our Cheapest Flight Search</p>
-//               </div>
-              
-//               {/* Enhanced Search Bar with Cube Icon */}
-//               <div className="search-bar">
-//                 {/* Cube Icon */}
-//                 <div className="cube-icon">
-//                   <FontAwesomeIcon icon={faCube} size="lg" />
-//                 </div>
-
-//                 <div className="search-item">
-//                   <div>
-//                     <p>From</p>
-//                     <h4>Washington D.C. (Any)</h4>
-//                   </div>
-//                   <FontAwesomeIcon icon={faPlaneDeparture} className="icon" />
-//                 </div>
-                
-//                 <div className="divider"></div>
-                
-//                 <div className="search-item">
-//                   <div>
-//                     <p>To</p>
-//                     <h4>Country, city or airport</h4>
-//                   </div>
-//                   <FontAwesomeIcon icon={faPlaneDeparture} className="icon" style={{ transform: 'rotate(45deg)' }} />
-//                 </div>
-                
-//                 <div className="divider"></div>
-                
-//                 <div className="search-item">
-//                   <div className="date-container">
-//                     <div>
-//                       <p>Depart</p>
-//                       <h4>Add date</h4>
-//                     </div>
-//                     <FontAwesomeIcon icon={faCalendarAlt} className="icon" />
-//                   </div>
-//                 </div>
-                
-//                 <div className="divider"></div>
-                
-//                 <div className="search-item">
-//                   <div className="date-container">
-//                     <div>
-//                       <p>Return</p>
-//                       <h4>Add date</h4>
-//                     </div>
-//                     <FontAwesomeIcon icon={faCalendarAlt} className="icon" />
-//                   </div>
-//                 </div>
-                
-//                 <div className="divider"></div>
-                
-//                 <div className="search-item">
-//                   <div>
-//                     <p>Travellers and cabin class</p>
-//                     <h4>1 Adult, Economy</h4>
-//                   </div>
-//                   <FontAwesomeIcon icon={faUsers} className="icon" />
-//                 </div>
-                
-//                 <button className="search-button">
-//                   <Link href="">
-//                     <FontAwesomeIcon icon={faSearch} /> Search
-//                   </Link>
-//                 </button>
-//               </div>
-//             </Carousel.Caption>
-//           </Carousel.Item>
-//         </Carousel>
-//       </section>
-
-//       {/* Footer */}
-      
-//     </>
-//   );
-// };
-
-// export default FlightsLanding;import React from "react";import React from 'react';
+import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import flightBanner from '../src/components/Banner/2.svg'; // Using the same banner image
+import flightBanner from '../src/components/Banner/2.svg';
 import Header from '../src/components/Common/Header/Header';
-// import Footer from '../src/components/Common/Footer/Footer';
-import './landing.css';
-import { Link, useForm } from '@inertiajs/react';
+import { Link } from '@inertiajs/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPlaneDeparture,
@@ -121,6 +10,7 @@ import {
   faUsers,
   faSearch,
 } from '@fortawesome/free-solid-svg-icons';
+import './landing.css';
 
 const FlightsLanding = () => {
   return (
@@ -142,87 +32,57 @@ const FlightsLanding = () => {
                 </p>
               </div>
 
-              {/* Search Bar Section with White Background */}
-              <div className="search-bar-container">
-                <div
-                  className="search-bar"
-                  style={{
-                   // backgroundColor: '#ffffff',  White background
-                    padding: '1rem', // Optional padding for spacing
-                    borderRadius: '8px', // Optional rounded corners
-                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Optional shadow for depth
-                  }}
-                >
-                  <div className="search-row">
-                    {/* From Field */}
-                    <div className="search-item">
-                      <div>
-                        <p>From</p>
-                        <h4>Washington D.C. (Any)</h4>
-                      </div>
-                      <FontAwesomeIcon icon={faPlaneDeparture} className="icon" />
-                    </div>
-
-                    {/* To Field */}
-                    <div className="filter-item">
-                      <div>
-                        <p>To</p>
-                        <h4>Country, city or airport</h4>
-                      </div>
-                      <FontAwesomeIcon
-                        icon={faPlaneDeparture}
-                        className="icon"
-                        style={{ transform: 'rotate(45deg)' }}
-                      />
-                    </div>
-
-                    {/* Depart Field */}
-                    <div className="filter-item">
-                      <div className="date-container">
-                        <div>
-                          <p>Depart</p>
-                          <h4>Add date</h4>
-                        </div>
-                        <FontAwesomeIcon icon={faCalendarAlt} className="icon" />
-                      </div>
-                    </div>
-
-                    {/* Return Field */}
-                    <div className="filter-item">
-                      <div className="date-container">
-                        <div>
-                          <p>Return</p>
-                          <h4>Add date</h4>
-                        </div>
-                        <FontAwesomeIcon icon={faCalendarAlt} className="icon" />
-                      </div>
-                    </div>
-
-                    {/* Travellers and Cabin Class Field */}
-                    <div className="filter-item">
-                      <div>
-                        <p>Travellers and cabin class</p>
-                        <h4>1 Adult, Economy</h4>
-                      </div>
-                      <FontAwesomeIcon icon={faUsers} className="icon" />
-                    </div>
-
-                    {/* Search Button */}
-                    <button className="bg-white px-4 py-2 rounded shadow hover:bg-gray-100 transition">
-                      {/* <Link href={route('flight_search')}> Adjust route as needed */}
-                        <FontAwesomeIcon icon={faSearch} /> Search
-                      {/* </Link> */}
-                    </button>
+              {/* Search Bar Section */}
+              <div className="bg-white p-4 rounded-lg shadow-md">
+                <div className="grid grid-cols-5 gap-4 items-center">
+                  {/* From Field */}
+                  <div className="search-item">
+                    <p>From</p>
+                    <h4>Washington D.C. (Any)</h4>
+                    <FontAwesomeIcon icon={faPlaneDeparture} className="icon" />
                   </div>
+
+                  {/* To Field */}
+                  <div className="search-item">
+                    <p>To</p>
+                    <h4>Country, city, or airport</h4>
+                    <FontAwesomeIcon icon={faPlaneDeparture} className="icon rotate-45" />
+                  </div>
+
+                  {/* Depart Field */}
+                  <div className="search-item">
+                    <p>Depart</p>
+                    <h4>Add date</h4>
+                    <FontAwesomeIcon icon={faCalendarAlt} className="icon" />
+                  </div>
+
+                  {/* Return Field */}
+                  <div className="search-item">
+                    <p>Return</p>
+                    <h4>Add date</h4>
+                    <FontAwesomeIcon icon={faCalendarAlt} className="icon" />
+                  </div>
+
+                  {/* Travellers & Cabin Class Field */}
+                  <div className="search-item">
+                    <p>Travellers & Cabin</p>
+                    <h4>1 Adult, Economy</h4>
+                    <FontAwesomeIcon icon={faUsers} className="icon" />
+                  </div>
+
+                  {/* Search Button */}
+                  <button className="col-span-5 bg-blue-600 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-700 transition">
+                    <Link href="#">
+                      <FontAwesomeIcon icon={faSearch} className="mr-2" />
+                      Search
+                    </Link>
+                  </button>
                 </div>
               </div>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </section>
-
-      {/* Footer */}
-      {/* <Footer /> */}
     </>
   );
 };
